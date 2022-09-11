ddcc14a6760b3a67aa394482f5c93d687258b60c
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-09-11 15:00:23 +0900

    add week1 & week2

---
+++ week1/W01_A.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#define vi vector<int> 
+
+using namespace std;
+
+void calcalate(vi* inhaKingdom, int inhaMin, vi* biryolKingdom, int biryolMin);
+int getGCD(vi* soldiers, int min);
+vi getDivisorArray(int min);
+int getFinal(vi* kingdom, int gcd);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t, n, m;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m;
+
+        vector<int> inhaKingdom = vector<int>(n);
+        vector<int> biryolKingdom = vector<int>(m);
+    
+        int t;
+        int inhaMin = 100000001;
+        for (int i = 0; i < n; i++) 
+        {
+            cin >> t;
+            inhaKingdom[i] = t;
+            inhaMin = min(inhaMin, t);
+        }
+
+        int biryolMin = 100000001;
+        for (int i = 0; i < m; i++)
+        {
+            cin >> t;
+            biryolKingdom[i] = t;
+            biryolMin = min(biryolMin, t);
+        }
+
+        calcalate(&inhaKingdom, inhaMin, &biryolKingdom, biryolMin);
+
+    }
+    
+}
+
+void calcalate(vi* inhaKingdom, int inhaMin, vi* biryolKingdom, int biryolMin)
+{
+    int inhaGCD = getGCD(inhaKingdom, inhaMin);
+    int biryolGCD = getGCD(biryolKingdom, biryolMin);
+
+    // 상대왕국의 GCD가 자신의 용병을 쓰러뜨리지 않는 것을 선택해야 함.
+    cout << getFinal(inhaKingdom, biryolGCD) << " " << getFinal(biryolKingdom, inhaGCD) << '\n';
+}
+
+int getGCD(vi* soldiers, int min) 
+{
+    vi divisorArray = getDivisorArray(min);
+    bool find;
+
+    for (int divisor : divisorArray)
+    {
+        find = true;
+        for (int soldier : *soldiers)
+        {
+            if (soldier % divisor != 0)
+            {
+                find = false;
+                break;
+            }
+        }
+
+        if (find)
+            return divisor;
+    }
+
+    return 1;
+}
+
+/*
+  가장 작은 수의 모든 약수을 구하는 함수
+  TimeComplexity : O(root N) => 유클리드 호제법을 이용하면 O(log N) 가능
+  return : 약수를 내림차순으로 리턴.
+*/
+vi getDivisorArray(int min) 
+{
+    vi divisorArray = vector<int>();
+    divisorArray.push_back(min);
+
+    for (int i = sqrt(min); i > 1; i--)
+    {
+        if (min % i == 0)
+        {
+            divisorArray.push_back(min/i);
+            divisorArray.push_back(i);
+        }
+        
+    }
+
+    sort(divisorArray.begin(), divisorArray.end(), [](int a, int b) -> int {
+        return a > b;
+        });
+
+    return divisorArray;
+}
+
+// 상대왕국의 GCD로, 자신의 용병을 쓰러뜨리지 않는 지 확인.
+int getFinal(vi* kingdom, int gcd)
+{
+    for (int soldier : *kingdom)
+        if (soldier % gcd == 0) return -1;
+    
+    return gcd;
+}
+
---
+++ week2/W02_A/W02_A.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct uint128 {
+    unsigned long long a;
+    unsigned long long b;
+};
+static uint128 stringToUint128(string input, int length);
+static char recoverByte(vector<uint128> alphabets, uint128 byte);
+static int getHammingDistance(uint128 a, uint128 b);
+
+int main()
+{
+
+    int t, m ,n;
+    
+    cin >> t;
+    while (t--)
+    {
+        cin >> m >> n;
+
+        string byte;
+        vector<uint128> alphabets;
+        for (int i = 0; i < m; i++)  // 시간복잡도는 O(n) * m = O(nm)
+        {
+            cin >> byte;
+            alphabets.push_back(stringToUint128(byte, n));
+        }
+        
+        int l;
+        uint128 current_byte_to_recover;
+        string bits;
+        cin >> l >> bits;
+        for (size_t i = 0; i < l/n; i++)  // 시간복잡도는 O(nm) * l/n = O(ml)
+        {
+            current_byte_to_recover = stringToUint128(bits.substr(i*n, n), n);
+            cout << recoverByte(alphabets, current_byte_to_recover);
+        }
+        cout << "\n";
+    }
+}
+
+/**
+ * 문자열로 주어진 비트 sequence 를 unsigned 128 bits 숫자형으로 변환하는 함수
+ * stooull 의 시간복잡도는 문자열 길이에 비례함 -> O(n)
+ * 문자열 길이 length를 n이라고 하면 총 시간복잡도는 O(n), (단 n은 [1, 128] 범위)
+ */
+uint128 stringToUint128(string input, int length)
+{
+    if (length <= 64)
+        return uint128({ 0, stoull(input,0,2) });
+    else
+        return uint128({
+                stoull(input.substr(0, length - 64),0,2),
+                stoull(input.substr(length - 64),0,2)
+            });
+}
+
+/**
+ * 주어진 byte 에 가장 근사한 알파벳을 골라내는 함수.
+ * byte 와 모든 알파벳의 거리를 계산한다.
+ * 알파벳의 개수를 m개, byte 의 비트 길이를 n 이라고 하면, 시간복잡도는 O(mn) 이다. 
+ * (단, m 은 [1, 26], n은 [1, 128] 범위이다.)
+ **/
+char recoverByte(vector<uint128> alphabets, uint128 byte)
+{
+    int min_dist = 99999, hamming_dist=99999, res = 0;
+    for (int i = 0; i < alphabets.size(); i++)
+    {
+        hamming_dist = getHammingDistance(alphabets[i], byte);
+        if (hamming_dist < min_dist)
+        {
+            min_dist = hamming_dist;
+            res = i;
+        }
+    }
+    return res + 65;
+}
+
+
+/**
+ * 주어진 두 bits sequence 의 Hamming distance 를 구하는 함수.
+ * (1) 두 수를 xor 하여 다른 bits 만 골라낸 다음,
+ * (2) 비트를 순회하면서 1의 개수를 센다.
+ * 
+ * 두 수의 bits 길이를 n라고 하면 O(n) 의 시간 복잡도를 갖는다. (단, n 는 [1, 128] 범위)
+ */
+int getHammingDistance(uint128 a, uint128 b)
+{
+    int dist = 0;
+    unsigned long long c[2] = {
+        a.a ^ b.a, 
+        a.b ^ b.b
+    };
+    
+    for (unsigned long long _c : c)
+    {
+        do {
+            if (_c & 1) dist += 1;
+        } while (_c >>= 1);
+    }
+
+    return dist;
+}
+
